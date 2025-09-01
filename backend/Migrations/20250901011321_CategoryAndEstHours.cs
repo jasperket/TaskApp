245cfdad04596d2915db9b51a38956d0234d09f8
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-01 09:15:49 +0800

    Add migrations for Category and EstHours

---
+++ backend/Migrations/20250901011321_CategoryAndEstHours.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryAndEstHours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EstimateHours",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CategoryId",
+                table: "Tasks",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Category_CategoryId",
+                table: "Tasks",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Category_CategoryId",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_CategoryId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "EstimateHours",
+                table: "Tasks");
+        }
+    }
+}
