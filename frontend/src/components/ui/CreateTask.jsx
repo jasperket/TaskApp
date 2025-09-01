1bfd464918528d1da5762637dddfd3c042533c3e
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-01 12:36:43 +0800

    Add fields to CreateTask

--- frontend/src/components/ui/CreateTask.jsx
+++ frontend/src/components/ui/CreateTask.jsx
@@ -1,33 +1,62 @@
 import useCreateTask from "../../hooks/useCreateTask";
 
-export default function CreateTask({ setError, loadTasks }) {
-  const { title, dueDate, saving, handleAdd, setTitle, setDueDate } =
+export default function CreateTask({ setError, loadTasks, categories }) {
+  const { title, dueDate, saving, handleAdd, setTitle, setDueDate, estimatedHours, setEstimatedHours, categoryId, setCategoryId } =
     useCreateTask(setError, loadTasks);
   return (
     <form
       onSubmit={handleAdd}
-      className="flex flex-wrap items-end gap-3 rounded-2xl bg-white p-4 shadow"
+      className="flex flex-col gap-3 rounded-2xl bg-white p-4 shadow"
     >
-      <div className="min-w-56 flex-1">
-        <label className="mb-1 block text-sm text-gray-600">Title *</label>
-        <input
-          className="w-full rounded-xl border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="Learn CRUD"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+      <div className="flex flex-1 gap-4">
+        <div className="min-w-56 flex-1">
+          <label className="mb-1 block text-sm text-gray-600">Title *</label>
+          <input
+            className="w-full rounded-xl border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Learn CRUD"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="flex-1">
+          <label className="mb-1 block text-sm text-gray-600">Category *</label>
+          <select
+            className="rounded-xl border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500 w-full"
+            value={categoryId}
+            onChange={(e) => setCategoryId(e.target.value)}
+          >
+            <option value="">Select a category</option>
+            {/* Category Loading */}
+            {categories.length === 0 && <option>Loading…</option>}
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
 
-      <div>
-        <label className="mb-1 block text-sm text-gray-600">Due date</label>
-        <input
-          type="date"
-          className="rounded-xl border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-        />
+      <div className="flex gap-4">
+        <div className="flex-1">
+          <label className="mb-1 block text-sm text-gray-600">Due date</label>
+          <input
+            type="date"
+            className="rounded-xl border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500 w-full"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+          />
+        </div>
+        <div className="flex-1">
+          <label className="mb-1 block text-sm text-gray-600">Estimated hours</label>
+          <input
+            type="number"
+            className="rounded-xl border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500 w-full"
+            value={estimatedHours}
+            onChange={(e) => setEstimatedHours(e.target.value)}
+          />
+        </div>
       </div>
-
       <button
         type="submit"
         disabled={saving}
--- frontend/src/hooks/useCreateTask.js
+++ frontend/src/hooks/useCreateTask.js
@@ -5,6 +5,8 @@ import { delay } from "../utils";
 export default function useCreateTask(setError, loadTasks) {
   const [title, setTitle] = useState("");
   const [dueDate, setDueDate] = useState(""); // YYYY-MM-DD from <input type="date">
+  const [estimatedHours, setEstimatedHours] = useState(0);
+  const [categoryId, setCategoryId] = useState(0);
   const [saving, setSaving] = useState(false); // shows button loading state
 
   // Handle create
@@ -12,6 +14,16 @@ export default function useCreateTask(setError, loadTasks) {
     e.preventDefault();
     if (!title.trim()) {
       setError("Title is required.");
+      return;
+    }
+
+    if (!categoryId) {
+      setError("Category is required.");
+      return;
+    }
+
+    if(estimatedHours < 0) {
+      setError("Estimated hours must be a positive number.");
       return;
     }
 
@@ -24,12 +36,16 @@ export default function useCreateTask(setError, loadTasks) {
         title: title.trim(),
         isDone: false,
         dueDate: dueDate || null,
+        estimatedHours: estimatedHours,
+        categoryId: categoryId,
       });
 
       // reset form + refresh list
       await delay(1000);
       setTitle("");
       setDueDate("");
+      setEstimatedHours(0);
+      setCategoryId(0);
       await loadTasks();
     } catch (err) {
       setError(err.message || "Failed to create task");
@@ -38,5 +54,5 @@ export default function useCreateTask(setError, loadTasks) {
     }
   }
 
-  return { title, dueDate, saving, handleAdd, setTitle, setDueDate };
+  return { title, dueDate, saving, handleAdd, setTitle, setDueDate, estimatedHours, setEstimatedHours, categoryId, setCategoryId };
 }
