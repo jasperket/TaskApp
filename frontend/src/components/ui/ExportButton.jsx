241a48ea1811b5a3223a0f9b7a11a0efc0d01a1e
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-02 11:39:37 +0800

    Column widths auto adjust

--- frontend/src/components/ui/ExportButton.jsx
+++ frontend/src/components/ui/ExportButton.jsx
@@ -24,6 +24,19 @@ export default function ExportButton({ tasks, categories }) {
     return data;
   }
 
+  function calculateWidths(data, columns) {
+    const widths = [];
+    for (const column of columns) {
+      console.log(column);
+      const max_width = data.reduce(
+        (w, r) => Math.max(w, r[column].length, column.length),
+        10,
+      );
+      widths.push({ wch: max_width });
+    }
+    return widths;
+  }
+
   async function exportData() {
     setExporting(true);
     await delay(1000);
@@ -33,20 +46,10 @@ export default function ExportButton({ tasks, categories }) {
     const worksheet = XLSX.utils.json_to_sheet(data);
 
     // Calculate column widths
-    const title_max_width = data.reduce(
-      (w, r) => Math.max(w, r.Title.length),
-      10,
-    );
-    const category_max_width = data.reduce(
-      (w, r) => Math.max(w, r.Category.length),
-      10,
-    );
+    const widths = calculateWidths(data, Object.keys(data[0]));
 
     // Set column widths
-    if (!worksheet["!cols"]) worksheet["!cols"] = [];
-    worksheet["!cols"][1] = { wch: title_max_width };
-    worksheet["!cols"][4] = { wch: "Estimated Hours".length };
-    worksheet["!cols"][5] = { wch: category_max_width };
+    if (!worksheet["!cols"]) worksheet["!cols"] = widths;
 
     // Add to workbook
     const workbook = XLSX.utils.book_new();
