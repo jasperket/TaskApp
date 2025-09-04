8e30cc820b88745cc249f5d29551f1a9c1b70952
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-04 10:45:58 +0800

    Cursor-pointer for SelectCategory

--- frontend/src/components/ui/SelectCategory.jsx
+++ frontend/src/components/ui/SelectCategory.jsx
@@ -18,7 +18,7 @@ export default function SelectCategory({
     setIsOpen(!isOpen);
   }
   return (
-    <div className="relative min-w-2xs">
+    <div className="relative min-w-2xs cursor-pointer">
       <div
         className={`flex w-full items-center justify-between rounded-xl border border-gray-300 px-3 py-2 outline-none ${isOpen && "ring-2 ring-blue-500"}`}
         onClick={handleIsOpen}
